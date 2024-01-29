296e9827400bde6bfb5d771e2d0e8effb212d69b
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-29 18:46:38 +0100

    Integrate Telegram Bot Logic into WebhooksController

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,10 +1,24 @@
 class WebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token
+
+  # def callback
+  #   # Logic to handle the Telegram updates [here]
+
+  #   # Responding to Telegram to acknowledge receipt
+  #   render json: { success: true }
+  # end
 
   def callback
-    # Logic to handle the Telegram updates [here]
-
-    # Responding to Telegram to acknowledge receipt
-    render json: { success: true }
+    token = Rails.application.credentials.telegram_bot[:token]
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        case message.text
+        when '/start'
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+        when '/stop'
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
+        end
+      end
+    end
+    head :ok
   end
 end
