3acd0eba0fcb19d65eac6db5694b4fc1ff72e0cf
Emma A. A. Rünzel <emma@ruenzel.de>
2024-02-07 19:33:17 +0100

    Update Environment Configs w/ Hosts & Logging Settings f/ Development & Production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,16 +1,16 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # NGROK Host
-  config.hosts << "7dbd-2003-d0-f3b-0-ed6b-d733-4868-eff5.ngrok-free.app"
+  # config.hosts << "7dbd-2003-d0-f3b-0-ed6b-d733-4868-eff5.ngrok-free.app"
+  config.hosts << /[a-z0-9-.]+\.ngrok-free\.app/
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
-
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -23,10 +23,10 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -59,7 +59,6 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
-
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
