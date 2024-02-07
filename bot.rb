67f11803de919709dc3128b92e20c31096340b2a
Emma A. A. Rünzel <emma@ruenzel.de>
2024-02-07 19:32:48 +0100

    Modify bot.rb w/ Search Feature & Response Handling f/ User Queries Testing

--- bot.rb
+++ bot.rb
@@ -1,14 +1,47 @@
 require 'telegram/bot'
 
+require 'httparty'
+require 'nokogiri'
+
 token = ENV['CHAT_BOT_TOKEN']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
+    if message.text.downcase.include?("start")
+      bot.api.send_message(
+        chat_id: message.chat.id, 
+        text: "👋 Hey there! 
+        Welcome to HandBot! 
+        We're here to help you find information about Germany through handbookgermany.de. 
+        Please select one of the following options to get started:
+
+        Press '/start' to begin your search 
+        Press '/help' if you don't get a proper response from the bot 
+
+        How can I help you?")
+    else
+      search_query = message.text
+      response = HTTParty.get("https://handbookgermany.de/en/search/content?keys=#{URI.encode(search_query)}")
+      parsed_content = Nokogiri::HTML(response.body)
+      articles = parsed_content.css('.item')[0..2]
+      if articles.empty?
+        bot.api.send_message(chat_id: message.chat.id, text: "I couldn't find any articles related to '#{search_query.downcase}'.")
+      else
+        articles_info = articles.map do |article|
+          title = article.css('h4').text.strip
+          url = article.css('a')['href']
+          "#{title}: #{url}"
+        end.join("\n")
+        bot.api.send_message(chat_id: message.chat.id, text: articles_info + "Was this what you were looking for?")
+      end
     end
   end
 end
+
+
+# Send a message to person w/ user ID
+# https://api.telegram.org/bot6431968803:AAEd4jpPVnTodP-TBFHYrhrjDbrhXRjNjqw/sendMessage?chat_id=262447015&text=Hello
+
+# https://api.telegram.org/bot6431968803:AAEd4jpPVnTodP-TBFHYrhrjDbrhXRjNjqw/sendMessage?chat_id=#{user_id}&text=#{text}
+
+# https://api.telegram.org/bot6431968803:AAEd4jpPVnTodP-TBFHYrhrjDbrhXRjNjqw/sendMessage?chat_id=#{group_id}&text=#{text}
