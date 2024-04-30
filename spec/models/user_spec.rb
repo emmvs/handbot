e3517fc27996ca0d5be44556bde8e45289bf2401
Emma A. A. Rünzel <emma@ruenzel.de>
2024-04-30 11:26:20 +0200

    Refactor User Specs w/ Different Users, Initialization & Validation Sections

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe User, type: :model do # rubocop:disable Metrics/BlockLength
-  before(:each) do
-    @user = User.new('123456789', 'private', 'testuser', 'en', '/start')
-  end
+  let(:valid_user) { User.new('123456789', 'private', 'testuser', 'en', '/start') }
+  let(:user_with_nils) { User.new(nil, nil, nil, nil, nil) }
+  let(:user_with_invalid_chat_id) { User.new('abc123', 'private', 'testuser', 'en', '/start') }
+  let(:user_with_unsupported_language) { User.new('123456789', 'private', 'testuser', 'xx', '/start') }
 
-  context 'when initialized with valid attributes' do
-    it 'initializes with correct attributes' do
-      expect(@user.chat_id).to eq('123456789')
-      expect(@user.chat_type).to eq('private')
-      expect(@user.username).to eq('testuser')
-      expect(@user.language_code).to eq('en')
-      expect(@user.command_input).to eq('/start')
+  describe 'Initialization' do
+    it 'initializes with correct attributes for a valid user' do
+      expect(valid_user.chat_id).to eq('123456789')
+      expect(valid_user.chat_type).to eq('private')
+      expect(valid_user.username).to eq('testuser')
+      expect(valid_user.language_code).to eq('en')
+      expect(valid_user.command_input).to eq('/start')
     end
-
-    it 'supports supported languages' do
-      expect(User::SUPPORTED_LANGUAGES.keys).to include(@user.language_code)
-    end
-  end
-
-  context 'when initialized with nil attributes' do
-    let(:user_with_nils) { User.new(nil, nil, nil, nil, nil) }
 
     it 'handles nil values gracefully' do
       expect(user_with_nils.chat_id).to be_nil
@@ -28,21 +21,25 @@ RSpec.describe User, type: :model do # rubocop:disable Metrics/BlockLength
       expect(user_with_nils.username).to be_nil
       expect(user_with_nils.command_input).to be_nil
     end
-  end
 
-  context 'when an unsupported language is provided' do
-    let(:user_with_unsupported_language) { User.new('123456789', 'private', 'testuser', 'xx', '/start') }
-
-    it 'falls back to the default language (en)' do
+    it 'sets default language when unsupported language is provided' do
       expect(user_with_unsupported_language.language_code).to eq(User::DEFAULT_LANGUAGE)
+    end
+
+    it 'retains the language when supported language provided' do
+      expect(valid_user.language_code).to eq('en')
     end
   end
 
-  context 'when a supported language is provided' do
-    let(:user_with_supported_language) { User.new('123456789', 'private', 'testuser', 'en', '/start') }
+  describe 'Validations' do
+    it 'checks chat_id format' do
+      expect(valid_user.send(:chat_id_valid?)).to be true
+      expect(user_with_invalid_chat_id.send(:chat_id_valid?)).to be false
+    end
 
-    it 'keeps the specified language' do
-      expect(user_with_supported_language.language_code).to eq('en')
+    it 'checks if the language is supported' do
+      expect(valid_user.language_supported?(valid_user.language_code)).to be true
+      expect(valid_user.language_supported?('xx')).to be false
     end
   end
 end
