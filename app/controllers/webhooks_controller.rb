c3f0d4b3989eafba5b385f0ddfba299d399471a7
Emma A. A. Rünzel <emma@ruenzel.de>
2024-05-16 16:14:19 +0200

    Refactor WebhooksController w/ TelegramClient f/ Post Requests

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -14,7 +14,7 @@ class WebhooksController < ApplicationController
       p "🟢 /message #{params[:message]}"
       handle_message(params[:message])
     elsif inline_query_request?
-      p "🟡 [inline query] #{params[:inline_query]}"
+      p "🔵 [inline query] #{params[:inline_query]}"
       handle_inline_query(params[:inline_query])
     else
       render_invalid_request
@@ -38,13 +38,13 @@ class WebhooksController < ApplicationController
   def handle_message(message)
     user = extract_user_info(message)
     response = TelegramCommandService.new(user).call
-    send_message(user.chat_id, response) unless response.nil?
+    TelegramClient.send_message(user.chat_id, response) unless response.nil?
   end
 
   def handle_inline_query(inline_query)
     user = extract_user_info(inline_query)
     results = SearchService.new(inline_query[:query], user).search
-    answer_inline_query(inline_query[:id], results)
+    TelegramClient.answer_inline_query(inline_query[:id], format_inline_results(results))
   end
 
   def extract_user_info(data)
@@ -57,17 +57,9 @@ class WebhooksController < ApplicationController
     User.new(chat_id, chat_type, name, language_code, command_input)
   end
 
-  def send_message(chat_id, text)
-    url = "https://api.telegram.org/bot#{ENV['CHAT_BOT_TOKEN']}/sendMessage"
-    HTTParty.post(url, body: { chat_id:, text: }.to_json, headers: { 'Content-Type' => 'application/json' })
-  end
-
-  def answer_inline_query(inline_query_id, results)
-    formatted_results = results.map do |result|
+  def format_inline_results(results)
+    results.map do |result|
       { type: 'article', id: result[:id], title: result[:title], input_message_content: { message_text: result[:url] } }
     end
-
-    url = "https://api.telegram.org/bot#{ENV['CHAT_BOT_TOKEN']}/answerInlineQuery"
-    HTTParty.post(url, body: { inline_query_id:, results: formatted_results.to_json }.to_json, headers: { 'Content-Type' => 'application/json' })
   end
 end
