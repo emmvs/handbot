166c6846ac89341fef93d5b514a4eae800f43acc
Emma A. A. Rünzel <emma@ruenzel.de>
2024-05-16 16:23:22 +0200

    Refactor CommandHandler f/ Improved Command Processing & Search Integration

--- app/services/command_handler.rb
+++ app/services/command_handler.rb
@@ -1,86 +1,67 @@
 # frozen_string_literal: true
 
-require_relative '../services/article_search_service'
-require_relative '../services/article_scrape_service'
-require_relative '../services/translation_service'
-
 # The CommandHandler handles all commands sent to the Telegram bot
 # (/start, /help, /search, /settings, /language) in a single, reusable component
 # Each public method within the service corresponds to a specific command,
 # simplifying the command processing workflow within the bot's controllers or other services
-class CommandHandler
+class CommandHandler < ApplicationService
+  HANDBOOK_BASE_URI = ENV['HANDBOOK_BASE_URI']
+
   attr_reader :user, :args, :user_language
 
   def initialize(user)
+    super()
     @user = user
   end
 
-  def process_command(command)
-    command_handler = {
-      '/start' => -> { handle_start },
-      '/search' => -> { handle_search },
-      '/help' => -> { handle_help },
-      '/settings' => -> { handle_settings },
-      '/language' => -> { handle_language }
-    }
+  def call(command)
+    return process_command(command) if command.start_with?('/')
 
-    handler = command_handler[command] || command_handler["/search_#{command}"] || method(:handle_unknown_command)
-    handler.call
+    perform_search(command) # Any non-command text is a search query
   end
 
   private
 
-  def handle_start
-    send_initial_greeting
+  def process_command(command) # rubocop:disable Metrics/MethodLength
+    case command
+    when '/start'
+      send_initial_greeting
+    when '/help'
+      translate('help_response')
+    when '/search'
+      translate('search_prompt')
+    when '/settings'
+      translate('settings_response')
+    when '/languages'
+      translate('change_language', languages: formatted_languages)
+    else
+      translate('unknown_command_response')
+    end
+  end
+
+  def perform_search(query)
+    search_service = SearchService.new(query, @user)
+    results = search_service.search
+    format_search_results(results, query)
   end
 
   def send_initial_greeting
     translation_key = 'initial_greeting'
-    interpolations = { username: @user.username }
+    interpolations = { username: @user.username, language_options: formatted_languages }
     translate(translation_key, interpolations)
   end
 
-  def handle_search
-    translate('search_prompt')
-  end
+  def format_search_results(results, query)
+    return translate('no_article_found', topic: query.capitalize) unless results.is_a?(Hash) && results[:articles]
 
-  def handle_search_with_keyword(command)
-    keyword = extract_keyword(command)
-    perform_search(keyword)
-  end
+    keyword = query
+    articles = results[:articles]
 
-  def extract_keyword(command)
-    command.split('_', 2).last
-  end
-
-  def perform_search(keyword)
-    results = search_articles(keyword)
-    results.empty? ? translate('no_article_found', topic: keyword.capitalize) : format_results(results, keyword)
-  end
-
-  def search_articles(keyword)
-    ArticleSearchService.search_by_keyword(keyword).presence || ArticleScrapeService.scrape_articles(keyword)
-  end
-
-  def handle_help
-    translate('help_response')
-  end
-
-  def handle_settings
-    translate('settings_response')
-  end
-
-  def handle_language
-    translate('language_options', languages: supported_languages_list)
-  end
-
-  def handle_unknown_command
-    translate('unknown_command_response')
-  end
-
-  def format_results(articles, keyword)
     articles_header = translate('articles_found', topic: keyword.capitalize)
-    articles_list = articles.map { |article| "#{article[:title]}: #{article[:link]}" }.join("\n")
+    articles_list = articles.map do |article|
+      "• #{article[:title]}: #{HANDBOOK_BASE_URI}#{article[:url]}"
+    end.join("\n")
+
     "#{articles_header}\n#{articles_list}"
   end
 
@@ -92,5 +73,9 @@ class CommandHandler
     User::SUPPORTED_LANGUAGES.map do |code, name|
       "#{name} (/#{code})"
     end.join(', ')
+  end
+
+  def formatted_languages
+    User::SUPPORTED_LANGUAGES.map { |code, language| "• #{language} (#{code})\n" }.join('')
   end
 end
