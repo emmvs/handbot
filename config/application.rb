b3539ce9b2c3048f0566ddc28b1fd1dbbfd8a247
Emma A. A. Rünzel <emma@ruenzel.de>
2024-06-03 08:00:32 +0200

    Update Locales w/ New Translations

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Handbot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Add lib to autoload paths
+    config.autoload_paths << Rails.root.join('lib')
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
