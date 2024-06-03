14e612d97d615c562385f0960670c16a4b2aa259
Emma A. A. Rünzel <emma@ruenzel.de>
2024-06-03 08:05:16 +0200

    Update Documentation from User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,19 @@
 # frozen_string_literal: true
 
-# User Model for managing user profile settings and session details
-# for interactions with the Telegram bot. This class encapsulates
-# user-related data, ensuring that all user information is validated
-# and conforms to the expected standards necessary for processing commands
-#
-# Attributes:
-#   @chat_id [String] Unique identifier for the user's chat session, typically numeric
-#   @chat_type [String] Type of chat, indicates whether it's a private, group, or channel chat
-#   @username [String] User's Telegram username or first name if username isn't set
-#   @language_code [String] ISO language code representing the user's preferred language
-#   @command_input [String] The last command or message input received from the user
-#
-# The model includes validation methods to ensure the presence and correctness
-# of critical attributes and checks for supported languages to cater to internationalization.
-#
+# The User Model manages user profile settings and session details
+# for interactions with the Telegram bot
 class User
   # DEFAULT_LANGUAGE = 'en'
   SUPPORTED_LANGUAGES = {
     'en' => 'English',
-    'de' => 'German',
-    'uk' => 'Ukrainian',
-    'ar' => 'Arabic',
-    'fa' => 'Persian',
-    'tr' => 'Turkish',
-    'fr' => 'French',
-    # 'ps' => 'Pashto',
-    'ru' => 'Russian'
+    'de' => 'Deutsch',
+    'uk' => 'Українська',
+    'tr' => 'Türkçe',
+    'fr' => 'Français',
+    'ru' => 'Русский',
+    # 'ps' => 'پښتو',
+    'ar' => 'العربية',
+    'fa' => 'فارسی'
   }.freeze
 
   attr_accessor :chat_id, :chat_type, :username, :language_code, :command_input
